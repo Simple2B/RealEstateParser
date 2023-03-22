2fbfca8f2c4554863891ebb9a31ec341fe3111c4
Nikolay <nchernov@gmail.com>
2023-03-22 16:06:46 +0200

    fix: config from .env

--- app/models/phone.py
+++ app/models/phone.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy.orm import mapped_column, Mapped, relationship
+from sqlalchemy.orm import mapped_column, Mapped
 from app import db
 from app.models.utils import ModelMixin
 
@@ -11,6 +11,6 @@ class Phone(db.Model, ModelMixin):
     id: Mapped[int] = mapped_column(primary_key=True)
     number = db.Column(db.String(256), unique=True, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now)
-    
+
     def __repr__(self):
         return f"<{self.id}:{self.number}>"
--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 
-base_dir = os.path.dirname(os.path.abspath(__file__))
+from dotenv import load_dotenv
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 
 class BaseConfig(object):
@@ -27,8 +30,13 @@ class BaseConfig(object):
     ADMIN_PASS = os.getenv("ADMIN_NAME", "admin")
     ADMIN_EMAIL = os.getenv("ADMIN_NAME", "admin@simple2b.net")
     REDIS_URL = os.getenv("REDIS_URL", "")
-    SEARH_QUERY = os.getenv("SEARH_QUERY", "")
+    SEARCH_QUERY = os.getenv("SEARCH_QUERY", "")
     MAX_PAGES_AMOUNT = int(os.getenv("MAX_PAGES_AMOUNT", "100"))
+
+    BASE_GOOGLE_GET = os.getenv(
+        "BASE_GOOGLE_GET",
+        "https://www.google.com/search?q={}&sxsrf=AJOqlzXEhSdrbbM-_fj3FpUJV-wRB_kX6w:1679489665710&ei=gfoaZI-KK9iPwPAP5MuPiAk&start=0&sa=N&filter=0&ved=2ahUKEwjPzrbHyu_9AhXYBxAIHeTlA5E4kAMQ8tMDegQIAxAE&biw=1710&bih=991&dpr=2",
+    )
 
     @staticmethod
     def configure(app):
@@ -42,7 +50,7 @@ class DevelopmentConfig(BaseConfig):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DEVEL_DATABASE_URL",
-        "sqlite:///" + os.path.join(base_dir, "database-devel.sqlite3"),
+        "sqlite:///" + os.path.join(BASE_DIR, "database-devel.sqlite3"),
     )
 
 
@@ -53,7 +61,7 @@ class TestingConfig(BaseConfig):
     PRESERVE_CONTEXT_ON_EXCEPTION = False
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "TEST_DATABASE_URL",
-        "sqlite:///" + os.path.join(base_dir, "database-test.sqlite3"),
+        "sqlite:///" + os.path.join(BASE_DIR, "database-test.sqlite3"),
     )
 
 
@@ -61,7 +69,7 @@ class ProductionConfig(BaseConfig):
     """Production configuration."""
 
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL", "sqlite:///" + os.path.join(base_dir, "database.sqlite3")
+        "DATABASE_URL", "sqlite:///" + os.path.join(BASE_DIR, "database.sqlite3")
     )
     WTF_CSRF_ENABLED = True
 
